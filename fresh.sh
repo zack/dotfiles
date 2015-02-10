0e96c25f40116e700a9bf4833afc61647689b3f0
Zack Youngren <zack@youngren.io>
2015-02-10 22:11:28 +0000

    starting a fresh install script

---
+++ fresh.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/antigen.git ~/dotfiles/antigen
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
