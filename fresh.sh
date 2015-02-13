1685fdc9f3347ddd8c9c620ea71d991b20bfa220
Zack Youngren <zack@youngren.io>
2015-02-12 22:36:18 -0500

    this might help

--- fresh.sh
+++ fresh.sh
@@ -2,4 +2,5 @@ git clone git@github.com:zack/dotfiles.git ~/dotfiles
 git clone https://github.com/zsh-users/antigen.git ~/dotfiles/antigen
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+sh ~/dotfiles/install_script
 vim - +PluginInstall +qal
