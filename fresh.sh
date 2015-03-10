ab098d7bc4354e0ceb7be0c5a33f11fc64393a7a
Zack Youngren <zack@youngren.io>
2015-03-10 10:44:17 -0400

    fix a stupid bug i somehow missed

--- fresh.sh
+++ fresh.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/zack/dotfiles.git ~/dotfiles
 git clone https://github.com/zsh-users/antigen.git ~/dotfiles/antigen
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/vim/bundle/Vundle.vim
+git clone https://github.com/scrooloose/syntastic.git ~/dotfiles/vim/bundle/syntastic
 sh ~/dotfiles/install_script
 vim - +PluginInstall +qal
