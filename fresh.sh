988522c98b5bd368532f2dbe168a60906aca5178
Zack Youngren <zack@youngren.io>
2015-04-08 12:11:27 -0400

    add vim-clojure-static to fresh.sh for pathogen

--- fresh.sh
+++ fresh.sh
@@ -2,5 +2,6 @@ git clone https://github.com/zack/dotfiles.git ~/dotfiles
 git clone https://github.com/zsh-users/antigen.git ~/dotfiles/antigen
 git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/vim/bundle/Vundle.vim
 git clone https://github.com/scrooloose/syntastic.git ~/dotfiles/vim/bundle/syntastic
+git clone https://github.com/guns/vim-clojure-static ~/dotfiles/vim/bundle/vim-clojure-static
 sh ~/dotfiles/install_script
 vim - +PluginInstall +qal
