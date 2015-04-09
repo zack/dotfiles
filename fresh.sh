b27f1943684aefabf61d463f94b85f345e722d31
Zack Youngren <zack@youngren.io>
2015-04-09 11:46:45 -0400

    conditionally download secrets correctly in fresh

--- fresh.sh
+++ fresh.sh
@@ -1,12 +1,11 @@
 OUTPUT=$(ssh -oStrictHostKeyChecking=no -T git@github.com 2>&1)
-# If you're name is zack and you aren't me ... sorry!
-if [[ $OUTPUT == *"zack"* ]]
-then
-  git clone git@github.com:zack/dotfiles.git ~/dotfiles
-  git clone git@github.com:zack/secrets.git ~/dotfiles/secrets
-else
-  git clone https://github.com/zack/dotfiles.git ~/dotfiles
-fi
+
+case $OUTPUT in
+*"zack"*) git clone git@github.com:zack/dotfiles.git ~/dotfiles &&
+          git clone git@github.com:zack/secrets.git ~/dotfiles/secrets ;;
+*       ) git clone https://github.com/zack/dotfiles.git ~/dotfiles ;;
+esac
+
 git clone https://github.com/zsh-users/antigen.git ~/dotfiles/antigen
 git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/vim/bundle/Vundle.vim
 git clone https://github.com/scrooloose/syntastic.git ~/dotfiles/vim/bundle/syntastic
