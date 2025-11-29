9e3f2217d328f2940edbbbc66c70eca22215e3cd
Zack Youngren <github.com@youngren.io>
2025-11-29 11:41:38 -0500

    Fix recording notifications in snacks.nvim

--- nvim/lua/plugins/snacks.nvim.lua
+++ nvim/lua/plugins/snacks.nvim.lua
@@ -2,11 +2,27 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  opts = {
-    notifier = { enabled = true },
-  },
-  keys = {
-    { "<leader>dn", function() require("snacks").notifier.hide() end, desc = "Dismiss All Notifications" },
-    { "<leader>hn", function() require("snacks").picker.notifications() end, desc = "Notification History" },
-  },
+  config = function ()
+    require("snacks").setup({
+      notifier = { enabled = true },
+      keys = {
+        { "<leader>dn", function() require("snacks").notifier.hide() end, desc = "Dismiss All Notifications" },
+        { "<leader>hn", function() require("snacks").picker.notifications() end, desc = "Notification History" },
+      },
+    })
+
+    vim.api.nvim_create_autocmd("RecordingEnter", {
+      callback = function()
+        local reg = vim.fn.reg_recording()
+        Last_recorded_reg = reg ~= "" and reg or "?"
+        require("snacks").notifier.notify("Recording started to: @" .. Last_recorded_reg, "info")
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("RecordingLeave", {
+      callback = function()
+        require("snacks").notifier.notify("Recording ended, stored in: @" .. Last_recorded_reg, "info")
+      end,
+    })
+  end
 }
