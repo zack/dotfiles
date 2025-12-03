ea6a8091cee8d21753af240bd2a501e330ea8536
Zack Youngren <github.com@youngren.io>
2025-12-03 16:09:39 -0500

    disable blink.cmp in comments using github suggestion

--- nvim/lua/plugins/blink.cmp.lua
+++ nvim/lua/plugins/blink.cmp.lua
@@ -1,3 +1,31 @@
+-- from https://github.com/Saghen/blink.cmp/discussions/564#discussioncomment-13439030
+function in_treesitter_capture(capture)
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  if vim.api.nvim_get_mode().mode == 'i' then
+    col = col - 1
+  end
+
+  local buf = vim.api.nvim_get_current_buf()
+  local get_captures_at_pos = require('vim.treesitter').get_captures_at_pos
+
+  local captures_at_cursor = vim.tbl_map(function(x)
+    return x.capture
+  end, get_captures_at_pos(buf, row - 1, col))
+
+  if vim.tbl_isempty(captures_at_cursor) then
+    return false
+  elseif type(capture) == 'string' and vim.tbl_contains(captures_at_cursor, capture) then
+    return true
+  elseif type(capture) == 'table' then
+    for _, v in ipairs(capture) do
+      if vim.tbl_contains(captures_at_cursor, v) then
+        return true
+      end
+    end
+  end
+  return false
+end
+
 return {
   'saghen/blink.cmp',
   dependencies = { 'rafamadriz/friendly-snippets' }, -- provide snippets for the snippet source
@@ -8,8 +36,14 @@ return {
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
     },
+    completion = {
+      menu = {
+        auto_show = function()
+          return not in_treesitter_capture("comment") and not require("luasnip").expand_or_jumpable()
+        end
+      }
+    },
     sources = {
-      -- find a way to disable this for comment writing...
       default = { 'lsp', 'path', 'snippets', 'buffer' },
       per_filetype = {
         codecompanion = { "codecompanion" },
