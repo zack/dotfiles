7bab1f25fc052abedf4b53f58eeca50b82dac505
Zack Youngren <github.com@youngren.io>
2025-12-02 20:09:11 -0500

    snacks indent and disable recording notifs for now, causing some issues

--- nvim/lua/plugins/snacks.nvim.lua
+++ nvim/lua/plugins/snacks.nvim.lua
@@ -3,25 +3,33 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    notifier = { enabled = true },
+    notifier = { enabled = true},
+    indent = { enabled = true },
+    animate = { -- animation settings for indent
+      enabled = vim.fn.has("nvim-0.10") == 1,
+      duration = {
+        step = 10, -- ms per step
+        total = 100, -- maximum duration
+      },
+    },
   },
   keys = {
     { "<Leader>dn", function() require("snacks").notifier.hide() end, desc = "Dismiss All Notifications" },
     { "<Leader>hn", function() require("snacks").picker.notifications() end, desc = "Notification History" },
   },
-  config = function ()
-    vim.api.nvim_create_autocmd("RecordingEnter", {
-      callback = function()
-        local reg = vim.fn.reg_recording()
-        Last_recorded_reg = reg ~= "" and reg or "?"
-        require("snacks").notifier.notify("Recording started to: @" .. Last_recorded_reg, "info")
-      end,
-    })
+  -- config = function ()
+    -- vim.api.nvim_create_autocmd("RecordingEnter", {
+      -- callback = function()
+        -- local reg = vim.fn.reg_recording()
+        -- Last_recorded_reg = reg ~= "" and reg or "?"
+        -- require("snacks").notifier.notify("Recording started to: @" .. Last_recorded_reg, "info")
+      -- end,
+    -- })
 
-    vim.api.nvim_create_autocmd("RecordingLeave", {
-      callback = function()
-        require("snacks").notifier.notify("Recording ended, stored in: @" .. Last_recorded_reg, "info")
-      end,
-    })
-  end
+    -- vim.api.nvim_create_autocmd("RecordingLeave", {
+      -- callback = function()
+        -- require("snacks").notifier.notify("Recording ended, stored in: @" .. Last_recorded_reg, "info")
+      -- end,
+    -- })
+  -- end
 }
