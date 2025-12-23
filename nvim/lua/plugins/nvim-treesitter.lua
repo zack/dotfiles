e3a0bbf9ff538e3852461e57283218b2a48c6fac
Zack Youngren <github.com@youngren.io>
2025-12-23 11:52:03 -0500

    update treesitter to main

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,30 @@
 return {
-  -- don't forget to `:TSInstall the following:
-  -- bash
-  -- javascript
-  -- jsx
-  -- lua
-  -- markdown
-  -- markdown_inline
-  -- prisma
-  -- regex
-  -- tsx
-  -- typescript
-  -- vim
   'nvim-treesitter/nvim-treesitter',
-  branch = 'master',
   lazy = false,
+  branch = 'main',
   build = ':TSUpdate',
   config = function()
-    require('nvim-treesitter.configs').setup {
+    require('nvim-treesitter').setup {
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
       },
     }
+
+    -- Can't get this to stop installing every time I open neovim
+    -- require('nvim-treesitter').install {
+      -- 'bash',
+      -- 'javascript',
+      -- 'jsx',
+      -- 'lua',
+      -- 'markdown',
+      -- 'markdown_inline',
+      -- 'prisma',
+      -- 'regex',
+      -- 'tsx',
+      -- 'typescript',
+      -- 'vim',
+    -- }
   end
 }
 
